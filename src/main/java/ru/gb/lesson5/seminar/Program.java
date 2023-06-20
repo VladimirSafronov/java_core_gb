a071987a2890a4e951214c9aa4db135526f6313a
VladimirSafronov <safron5000@mail.ru>
2023-06-20 23:12:57 +0300

    hw seminar05

--- src/main/java/ru/gb/lesson5/seminar/Program.java
+++ src/main/java/ru/gb/lesson5/seminar/Program.java
@@ -30,21 +30,21 @@ public class Program {
     System.out.println(isContainsInFile("sample02.txt", WORD_FOR_SEARCH));
     concatenateFiles("sample01.txt", "sample02.txt", "sampleConc.txt");
     System.out.println(isContainsInFile("sampleConc.txt", WORD_FOR_SEARCH));
-//
-//    String[] fileNames = new String[10];
-//    for (int i = 1; i < 6; i++) {
-//      fileNames[i] = "file_" + i + ".txt";
-//      writeFileContents(fileNames[i], 100, 4);
-//      System.out.printf("Файл %s создан\n", fileNames[i]);
-//    }
-//
-//    List<String> res = searchMatch(fileNames, WORD_FOR_SEARCH);
-//
-//    for (String file : res) {
-//      System.out.printf("Файл %s содержит искомое слово '%s'\n", file, WORD_FOR_SEARCH);
-//    }
 
-//    Tree.print(new File("."), "", true);
+    String[] fileNames = new String[10];
+    for (int i = 1; i < 6; i++) {
+      fileNames[i] = "file_" + i + ".txt";
+      writeFileContents(fileNames[i], 100, 4);
+      System.out.printf("Файл %s создан\n", fileNames[i]);
+    }
+
+    List<String> res = searchMatch(fileNames, WORD_FOR_SEARCH);
+
+    for (String file : res) {
+      System.out.printf("Файл %s содержит искомое слово '%s'\n", file, WORD_FOR_SEARCH);
+    }
+
+    Tree.print(new File("."), "", true);
 
     copyFiles(new File("/Users/vladimirsafronov/Desktop/it"));
   }
